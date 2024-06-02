203ab1586d9ee9c1f724e77dd3301bd266efac13
Nathan Mande <neodarksoulink@gmail.com>
2024-06-02 12:13:53 +0200

    feat: autowire events

--- src/shared/infrastructure/event-store/deserializers/event.deserializer.ts
+++ src/shared/infrastructure/event-store/deserializers/event.deserializer.ts
@@ -2,7 +2,7 @@ import { Injectable, Type } from '@nestjs/common';
 
 import { SerializableEvent } from 'src/shared/domain/interfaces/serializable-event';
 import { Event } from '../schemas/event.schema';
-import { AlarmCreatedEvent } from 'src/alarms/domain/events/alarm-created.event';
+import { EventClsRegistry } from '../event-cls.registry';
 
 @Injectable()
 export class EventDeserializer {
@@ -16,10 +16,7 @@ export class EventDeserializer {
   }
 
   getEventClassByType(type: string) {
-    switch (type) {
-      case AlarmCreatedEvent.name:
-        return AlarmCreatedEvent;
-    }
+    return EventClsRegistry.get(type);
   }
 
   instantiateSerializedEvent<T extends Type>(
--- src/shared/infrastructure/shared-infrastructure.module.ts
+++ src/shared/infrastructure/shared-infrastructure.module.ts
@@ -1,13 +1,13 @@
 import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 
-import { EventSchema } from './event-srore/schemas/event.schema';
+import { EventSchema } from './event-store/schemas/event.schema';
 import { EVENT_STORE_CONNECTION } from '../../core/core.constants';
-import { EventSerializer } from './event-srore/serializers/event.serializer';
-import { EventStorePublisher } from './event-srore/publishers/event-store.publisher';
-import { MongoEventStore } from './event-srore/mongo-event-store';
-import { EventsBridge } from './event-srore/events-bridge';
-import { EventDeserializer } from './event-srore/deserializers/event.deserializer';
+import { EventSerializer } from './event-store/serializers/event.serializer';
+import { EventStorePublisher } from './event-store/publishers/event-store.publisher';
+import { MongoEventStore } from './event-store/mongo-event-store';
+import { EventsBridge } from './event-store/events-bridge';
+import { EventDeserializer } from './event-store/deserializers/event.deserializer';
 
 @Module({
   imports: [
